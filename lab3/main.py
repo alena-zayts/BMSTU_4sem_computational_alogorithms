f9f2545c0ef33d93c03596d6204a449cd6e0d4b8
Alena Zaytseva <64847038+zaa19u450@users.noreply.github.com>
2021-03-31 11:45:28 +0300

    Add files via upload

--- lab3/main.py
+++ lab3/main.py
@@ -8,49 +8,103 @@ def make_table(N=11):
     y = x ** 2
     return {'x': x, 'y': y}
 
-# найти кси(i+1)
-def find_xi_iplus1(h_i, h_iminus1, xi_i):
-    return (-h_i) / (h_iminus1 * xi_i +
-                     2 * (h_iminus1 + h_i))
-
-def find_eta_iplus1(f_i, h_):
-    print()
-
-
+# создаем таблицу
 data = make_table()
-# i = [1, N - 1]
 N = len(data['x'])
+
+# находим hi
 data['h'] = [None] + list(data['x'][i] - data['x'][i - 1]
                           for i in range(1, N))
-# i = [2, N-1]
+# находим fi
 data['f'] = [None, None] + list(3 * (((data['y'][i] - data['y'][i - 1]) / data['h'][i]) -
                                 ((data['y'][i - 1] - data['y'][i - 2]) / data['h'][i - 1]))
                                 for i in range(2, N))
-# i = [1, N-1]
-data['a'] = [None] + list(data['x'][i - 1] for i in range(1, N))
-
-# i = [2, N]
+# прямой ход: при известных кси2=0 и эта2=0, находим все прогоночные коэффициенты
 data['xi'] = [None, None] + [0]
 for i in range(3, N + 1):
     xi_i = -data['h'][i - 1] / (data['h'][i - 2] * data['xi'][i - 1] +
                      2 * (data['h'][i - 2] + data['h'][i - 1]))
     data['xi'].append(xi_i)
 
-# i = [2, N]
 data['eta'] = [None, None] + [0]
 for i in range(3, N + 1):
     eta_i = ((data['f'][i - 1] - data['h'][i - 2] * data['eta'][i - 1]) /
              (data['h'][i - 2] * data['xi'][i - 1] + 2 * (data['h'][i - 2] + data['h'][i - 1])))
     data['eta'].append(eta_i)
 
+# обратные ход: при условии c[N+1]=0, определяем все коэффициенты c
+# с помощью прогоночных коэффициентов
 data['c'] = [None] + [0] * (N)
 for i in range(N - 1, 0, -1):
     data['c'][i] = data['xi'][i + 1] * data['c'][i + 1] + data['eta'][i + 1]
 
-#data['xi'] = ([None, None] + [0] +
-#              list((-data['h'][i - 1]) / (data['h'][i - 2] * data['xi'][i - 1] +
-#                     2 * (data['h'][i - 2] + data['h'][i - 1])) for i in range(3, N + 1)))
-for l in data.values():
-    print(l)
+# с помощью коэффициентов c находим коэффициенты b и d
+data['b'] = [None] + list((data['y'][i] - data['y'][i - 1]) / data['h'][i] -
+                          data['h'][i] * (data['c'][i + 1] - 2 * data['c'][i]) / 3
+                          for i in range(1, N))
+
+data['d'] = [None] + list((data['c'][i + 1] - data['c'][i]) / 3 / data['h'][i]
+                          for i in range(1, N))
+
+# находим все коэффициенты a из условия, что в узлах значения многочлена и интерполируемой функции совпадают
+data['a'] = [None] + list(data['y'][i - 1] for i in range(1, N))
 
 
+# поиск коэффициентов полинома на участке, в котором находится точка x
+# а также точки, с которой начинается этот участок
+def choose_coeffs(data, x):
+    i_beg = 0
+    for i in range(1, N - 1):
+        if x < data['x'][i]:
+            i_beg = i
+            break
+    return [data['a'][i_beg], data['b'][i_beg], data['c'][i_beg], data['d'][i_beg],
+            data['x'][i_beg - 1]]
+
+# подсчет значения полинома в точке x на участке, начинающемся с точки x0
+def count_polynom3(a, b, c, d, x0, x):
+    return (a + b * (x - x0) + c * (x - x0) ** 2 +
+            d * (x -x0) ** 3)
+
+# вводим значения x и интерполируем кубическим сплайном
+x1 = 0.5
+coeffs1 = choose_coeffs(data, x1)
+res_spline1 = count_polynom3(*coeffs1, x1)
+
+x2 = 5.5
+coeffs2 = choose_coeffs(data, x2)
+res_spline2 = count_polynom3(*coeffs2, x2)
+
+# интерполируем полиномом Ньютона 3 степени (lab1.py - код из 1 ЛР)
+import lab1
+res_newton1 = lab1.approximate_newton(data['x'], data['y'], 3, x1)
+res_newton2 = lab1.approximate_newton(data['x'], data['y'], 3, x2)
+
+# подсчитываем настоящее значение
+res_real1 = x1 ** 2
+res_real2 = x2 ** 2
+
+# вывод и сравнение результатов
+print('Точка:', x1)
+print('Интерполяция кубическим сплайном:', res_spline1)
+print('Интерполяция полиномом Ньютона 3 степени:', res_newton1)
+print('Настоящее значение:', res_real1)
+print()
+
+print('Точка:', x2)
+print('Интерполяция кубическим сплайном:', res_spline2)
+print('Интерполяция полиномом Ньютона 3 степени:', res_newton2)
+print('Настоящее значение:', res_real2)
+
+
+x3 = 1.9
+coeffs3 = choose_coeffs(data, x3)
+res_spline3 = count_polynom3(*coeffs3, x3)
+res_newton3 = lab1.approximate_newton(data['x'], data['y'], 3, x3)
+res_real3 = x3 ** 2
+
+print('\nТочка:', x3)
+print('Интерполяция кубическим сплайном:', res_spline3)
+print('Интерполяция полиномом Ньютона 3 степени:', res_newton3)
+print('Настоящее значение:', res_real3)
+print()
